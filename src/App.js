3e80f8797d877ca58463632c94dd80367a0f971e
RAMESH <140529732+RameshRamST@users.noreply.github.com>
2024-07-18 22:12:27 +0530

    x adn o done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'; 
+import { useState } from 'react'
+function Button({ value, initChange }) {
+    return (<button className="ind-square" onClick={initChange}>{value}</button>);
 }
 
-export default App;
+
+
+export default function MyApp() {
+  const [isx, setIsX] = useState(true);
+  const [box, setBox] = useState(Array(9).fill(null));
+
+
+  
+  function manage(i) {
+    
+    const tempe = box.slice();
+    if (box[i]||isFind(i)) {
+      return;
+    }
+    if (isx) {
+      tempe[i] = "X";
+    } else {
+      tempe[i] = "O";
+    }
+    setIsX(!isx);
+    setBox(tempe);
+
+  }
+  function isFind() {
+  const lines=[ [0,1,2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]]
+  for ( let j = 0; j < lines.length; j++){
+    const[a,b,c]=lines[j]
+    if (box[a] && box[b] === box[a] && box[a] === box[c]) {
+      return (box[a]);
+    }
+    
+     
+    
+  }
+     return null;
+  }
+
+  const winner = isFind();
+  let status;
+    if (winner) {
+      status=winner+"is the winner"
+    } else {
+      status = "next paly is " + (isx ? "X":"o")
+    }
+   
+
+  
+
+  return (
+    <div className="big-box">
+      <div className="nothing">{status}</div>
+      <div className='border-row'>
+        <Button value={box[0]} initChange={() => manage(0)} />
+        <Button value={box[1]} initChange={() => manage(1)} />
+        <Button value={box[2]} initChange={() => manage(2)} />
+      </div>
+      <div className='border-row'>
+        <Button value={box[3]} initChange={() => manage(3)} />
+        <Button value={box[4]} initChange={() => manage(4)} />
+        <Button value={box[5]} initChange={() => manage(5)} />
+      </div>
+      <div className='border-row'>
+        <Button value={box[6]} initChange={() => manage(6)} />
+        <Button value={box[7]} initChange={() => manage(7)} />
+        <Button value={box[8]} initChange={() => manage(8)} />
+      </div>
+    </div>
+  );
+
+}
